52cdfcb569a969ff090d087e65923a21af963e87
Apxy <ckc.zachary@yahoo.com>
2023-03-12 16:05:06 +0800

    minor bugfix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Incorrectcontextprovider } from './store/incorrectcontext';
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
-    <Incorrectcontextprovider>
+
 <App /> 
-</Incorrectcontextprovider>,
+,
 document.getElementById('root'));
 
 
