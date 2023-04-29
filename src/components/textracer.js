17582db51257d33184d9b4ab36b5addbafc7fa47
Replit user <>
2023-04-29 02:51:56 +0000

    Revert "fixed bugs for best run stored locally; however there is still a bug with strings yet to be fixed"
    This reverts commit 43f8e2b6b6d7a62b8072eac7d73a00136ae0eade.

--- src/components/textracer.js
+++ src/components/textracer.js
@@ -429,18 +429,15 @@ function Typingengine() {
     if (replayracestate || replayinitstate) { return; }
     setreplaybtnstate(false);
   }
-  const replayagainstself = () => {
-    setbestrunstate(JSON.parse(localStorage.getItem("bestrun"))); //this is the function that clicking the replay against self button calls
+  const replayagainstself = () => { //this is the function that clicking the replay against self button calls
     setreplaystate(true);
     origstr = bestrunstate.str;
     handleClearInput();
-    setracestatus("replay");
     //CountdownTimer();
     //alert(bestrunstate);
     setreplayinitstate(true);
     //Replaybesttext(); 
     document.getElementById("name-input").type = "text";
-    setreplaybtnstate(false);
   }
 
 
